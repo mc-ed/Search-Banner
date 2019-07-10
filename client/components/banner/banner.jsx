3e23d1b17610f2770f1501844d4a80077e6408ba
DJ Park <dongjae93@gmail.com>
2019-07-10 16:46:00 -0500

    shopping cart and user login sign is completed

--- client/components/banner/banner.jsx
+++ client/components/banner/banner.jsx
@@ -12,11 +12,48 @@ const Banner = (props) => {
               <div style={{'color': '#0471af'}}>
                 Visit Lowe's Today!<a className="lowes-icon "> {"\uEDBF"}</a>
               </div>
-              
             </div>
           </div>
         </div>
-
+        <div className="col-6">
+          <div className="row row-adjust">
+            <div className="col-7"/>
+            <div className="col-3 destroy-padding">
+              <div className="row row-adjust user-cart">
+                <div className="col-6 usericon-wrapper">
+                  <div className="destroy-padding lowes-icon usericon">
+                    {'\u0050'}
+                  </div>
+                </div>
+                  <div className="col-6 destroy-padding signin-wrapper">
+                    <span>
+                      my
+                    </span>
+                    <span className="bold-lowes">
+                    {"lowes \n"}
+                    </span>
+                    <span className="signin-font">
+                      Sign in
+                    </span>
+                  </div>
+              </div>
+            </div>
+            <div className="col-2 column-adjust">
+              <div className="row row-adjust user-cart">
+                <div className="col-6 destroy-padding carticon-wrapper">
+                  <div className="destroy-padding lowes-icon usericon">
+                    {'\u007D'}
+                  </div>
+                </div>
+                <div className="col-6 column-adjust destroy-padding-left destroy-padding-right carticon-wrapper">
+                  <div className="cart-item-number">
+                    0
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     
    );

5543c6eef8bfc89b748e8e3da8bc665b33c1e249
DJ Park <dongjae93@gmail.com>
2019-07-10 10:35:35 -0500

    deploying to docker

--- server/index.js
+++ server/index.js
@@ -7,6 +7,13 @@ const db = require('../db/index.js');
 const cors = require('cors')
 
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 app.use(bodyparser.json());
 app.use(express.static(path.join(__dirname, '../public')));
 

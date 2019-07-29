9c023e80ae13990de10a8adf7d592ff09da9de1a
Daxo <17711952+Daxo@users.noreply.github.com>
2019-07-29 16:49:40 -0500

    Added script to PSQL to added ts_vector index

--- db/data/itemsPostSeed.sql
+++ db/data/itemsPostSeed.sql
@@ -5,3 +5,11 @@ ALTER COLUMN itemname TYPE VARCHAR,
 ALTER COLUMN category TYPE VARCHAR,
 ALTER COLUMN views TYPE INTEGER USING views::integer,
 ALTER COLUMN itemid TYPE INTEGER USING itemid::integer;
+
+ALTER TABLE items ADD "document_vectors" tsvector;
+CREATE INDEX idx_fts_doc_vec ON items USING gin(document_vectors);
+
+UPDATE 
+    items 
+SET 
+    document_vectors = (to_tsvector(itemname));
